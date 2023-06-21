f8e34cbc25a7cc07f4f3e7d5865b29feab9610d9
JuanjoA <jalgaz@gmail.com>
2023-06-21 16:17:20 +0200

    test mod369 ok

--- l10n_es_aeat_mod369/__manifest__.py
+++ l10n_es_aeat_mod369/__manifest__.py
@@ -3,12 +3,12 @@
 
 {
     "name": "AEAT modelo 369",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Accounting",
     "author": "Studio73, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
     "license": "AGPL-3",
-    "depends": ["l10n_es_aeat", "l10n_eu_oss"],
+    "depends": ["l10n_es_aeat", "l10n_eu_oss_oca"],
     "data": [
         "data/tax_code_map_mod369_data.xml",
         "data/2022/aeat.model.export.config.csv",
